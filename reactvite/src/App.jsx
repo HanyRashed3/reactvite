d63125e208546f543221461640a04d256152ad79
HanyRashed3 <hanyrashed333@gmail.com>
2025-05-03 12:19:25 +0300

    1

---
+++ reactvite/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function App() {
+  
+
+  return (
+
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+export default App;
